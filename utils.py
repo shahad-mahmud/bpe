2ece2d763b3f9f1f73bbbfeedc75cd2255cf61cf
Shahad Mahmud <shahad9381@gmail.com>
2021-10-01 11:03:00 +0600

    customable end token

--- utils.py
+++ utils.py
@@ -19,8 +19,8 @@ def create_vocab_from_file(file_path: str, end_token: str = '_') -> collections.
             
             for word in words:
                 # Join the letters of a word separeted by a space.
-                # Add a `_` at the end. Finally, increase the count 
-                # of this token.
+                # Add a `end_token` at the end. Finally, increase 
+                # the count of this token.
                 vocab[' '.join(list(word)) + f' {end_token}'] += 1
     return vocab
 
